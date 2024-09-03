5a2a51f69a940c4e6071b1b4c82cf2a8a83f8711
Jonas Sebastian <jonas.sebastian@dlsau.edu.ph>
2024-09-04 00:14:04 +0800

    initial commit

--- practice_ecommerce_backend/settings.py
+++ practice_ecommerce_backend/settings.py
@@ -47,6 +47,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
 ]
 
 ROOT_URLCONF = 'practice_ecommerce_backend.urls'
