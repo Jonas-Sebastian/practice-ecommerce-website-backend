903ee1656edb123dd04fd232a403637b98c1309c
Jonas Sebastian <jonas.sebastian@dlsau.edu.ph>
2026-03-10 23:33:59 +0800

    Update Order Serializer to mask sensitive fields

--- practice_shop/serializers.py
+++ practice_shop/serializers.py
@@ -39,6 +39,14 @@ class OrderSerializer(serializers.ModelSerializer):
         model = Order
         fields = ['id', 'display_id', 'customer_name', 'customer_email', 'shipping_address', 
                   'order_notes', 'payment_method', 'created_at', 'status', 'order_items', 'status_choices']
+        extra_kwargs = {
+            'customer_name': {'write_only': True},
+            'customer_email': {'write_only': True},
+            'shipping_address': {'write_only': True},
+            'status': {'write_only': True},
+            'order_notes': {'write_only': True},
+            'payment_method': {'write_only': True},
+        }
 
     def create(self, validated_data):
         order_items_data = validated_data.pop('order_items')
